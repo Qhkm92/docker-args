143d9205eaf6d9df92f6bfb27345466ddd02e142
Qaiyyum Hakimi <qaiyyumhakimi@gmail.com>
2020-05-02 14:44:48 +0900

    Add comments

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM node:12
 
+# catch args from docker build 
 ARG programid
 
+# set it to docker env
 ENV PROGRAM_ID=$programid
 
 WORKDIR /usr/src/app
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.listen(PORT)
 
+// The passed env will be available and we can access it like this
 console.log(process.env.PROGRAM_ID);
 
 console.log(`running on port ${PORT}`)
